8ce8ed4344965864b368a8ad6adee50be069199c
Kirill Egorov <kirill.egorov@x5.ru>
2024-02-01 16:31:10 +0200

    fixed mobile height by react-div-100vh

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-import { useMobile } from "./hooks/useMobile";
+import { use100vh } from "react-div-100vh";
 
 import Game from "./Game";
 
 import "./App.css";
+import { useMobile } from "./hooks/useMobile";
 
 function App() {
   const isMobile = useMobile();
+  const height = use100vh();
+
   return (
     <div
       className="game"
-      style={{ height: isMobile ? window.innerHeight : "100vh" }}
+      style={{ height: isMobile ? `${height}px` : "inherit" }}
     >
       <header className="header">Snakemaster</header>
       <Game />
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Div100vh from "react-div-100vh";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Div100vh>
+      <App />
+    </Div100vh>
   </React.StrictMode>
 );
 
