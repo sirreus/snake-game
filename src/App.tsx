6ba9fb6660bab9c7dbadbf7ce84e0e26a2ade53b
Kirill Egorov <kirill.egorov@x5.ru>
2024-02-01 16:03:52 +0200

    fixed mobile height

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-import Game from "./Main";
+import { useMobile } from "./hooks/useMobile";
+
+import Game from "./Game";
 
 import "./App.css";
 
 function App() {
+  const isMobile = useMobile();
   return (
-    <div className="game">
+    <div
+      className="game"
+      style={{ height: isMobile ? window.innerHeight : "100vh" }}
+    >
       <header className="header">Snakemaster</header>
       <Game />
     </div>
