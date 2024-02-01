f671b44d270a9546c69893728a531bdec1b9b4a9
Kirill Egorov <kirill.egorov@x5.ru>
2024-02-01 16:44:22 +0200

    typos

--- src/components/Dialog/components/GameDescription.tsx
+++ src/components/Dialog/components/GameDescription.tsx
@@ -7,7 +7,7 @@ interface IGameDescription {
 export const GameDescription: React.FC<IGameDescription> = ({ isMobile }) => (
   <div className="game-description">
     <p className="dialog-text start">
-      <b>GAME OBJECT: </b>Eat all food on playground to raise your snake.
+      <b>GAME OBJECTIVE: </b>Eat all food on playground to raise your snake.
     </p>
 
     {isMobile ? (
@@ -30,12 +30,12 @@ export const GameDescription: React.FC<IGameDescription> = ({ isMobile }) => (
     )}
 
     <p className="dialog-text start">
-      <b>END GAME: </b>
+      <b>YOU LOSE IF: </b>
     </p>
     <ul style={{ margin: "0 0 8px 0" }}>
-      <li className="dialog-text start">if the snake eats itself</li>
+      <li className="dialog-text start">the snake eats itself</li>
       <li className="dialog-text start">
-        if the snake crosses the field's border
+        the snake crosses the field's border
       </li>
     </ul>
   </div>
